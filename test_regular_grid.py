ee8844a8f369f5ac2a756053f1c0b8dfc8dee647
h.wenzel <you@example.com>
2025-11-19 16:04:01 +0100

    test for RGI

--- test_regular_grid.py
+++ test_regular_grid.py
@@ -1,100 +1,68 @@
 #  %%
 import pathlib
-
 import numpy as np
 import pandas as pd
-
 from scipy.interpolate import RegularGridInterpolator
 from matplotlib import pyplot
 
 current_dir = pathlib.Path(__file__).parent
 
 # get data
-dac_data = pd.read_csv(current_dir.joinpath("dac_data.csv"))
+dac_data = pd.read_csv(current_dir.joinpath("dac_data.csv"), index_col=0)
 point_1_T_ = dac_data.loc[:, "T"]
 point_2_RH_ = dac_data.loc[:, "RH"]
 total_electricity = dac_data.loc[:, "totalElectricity"]
 relative_humidity = np.genfromtxt(
     fname=current_dir.joinpath("relative_humidity.csv"), delimiter=","
 )
+
 surface_air_temperature = np.genfromtxt(
     current_dir.joinpath("surface_air_temperature.csv"), delimiter=","
 )
 
+#2nd dac dataset:
+dac_data2 = pd.read_csv(current_dir.joinpath("LT_sendi.csv"), index_col=0)
+
+
+# %% 
 # Define interpolation
-selected_slice = slice(0, 140)
 
-# elec = griddata(
-#     points=(point_1_T_.loc[selected_slice], point_2_RH_.loc[selected_slice]),
-#     values=total_electricity.loc[selected_slice],
-#     xi=(
-#         surface_air_temperature[selected_slice, :],
-#         relative_humidity[selected_slice, :],
-#     ),
-#     method="linear",
-# )
+#For RegularGridInterpolator, the data needs unique x/y vectors with a corresponding z matrix
+dac_data_pivot = dac_data.pivot_table(values='totalElectricity', index='T', columns='RH')
 
-# linear_interpolator = LinearNDInterpolator(
-#     list(zip(point_1_T_.loc[selected_slice], point_2_RH_.loc[selected_slice])),
-#     total_electricity.loc[selected_slice],
-# )
-# linear_interpolator_output = linear_interpolator(
-#     surface_air_temperature[selected_slice, :], relative_humidity[selected_slice, :]
-# )
+#RGI:
+values = dac_data_pivot.values
+points = dac_data_pivot.index.values, dac_data_pivot.columns.values.astype('float64')
+RGI = RegularGridInterpolator(points=points, values=values)
+RGI_output = RGI((
+    surface_air_temperature,
+    relative_humidity,
+))
 
-combined_data_set = pd.DataFrame(
-    data=[point_1_T_, point_2_RH_, total_electricity]
-).transpose()
-sorted_df = combined_data_set.sort_values(by=["T", "RH"])
-
-
-x_unique = np.sort(combined_data_set["T"].unique())
-y_unique = np.sort(combined_data_set["RH"].unique())
-
-# 2. Pivot to 2D grid
-Z = (
-    combined_data_set.pivot(index="T", columns="RH", values="totalElectricity")
-    .reindex(index=x_unique, columns=y_unique)
-    .values
-)
-
-# 3. Build interpolator
-f = RegularGridInterpolator((x_unique, y_unique), Z)
-
-
-regular_interpolator_output = f(
-    (
-        surface_air_temperature[selected_slice, :],
-        relative_humidity[selected_slice, :],
-    )
-)
-# pass
-# regular_grid_inerpolator.[
-#     surface_air_temperature[selected_slice, :],
-#     relative_humidity[selected_slice, :],
-# ]
-# points sind (x,y) punkte
-# Values sind f(x,y)
-# xi sind die stellen wo interpoliert wird
-# Compare interpolation data
+#Load testdata (from old, griddata() interpolation)
 elec_test_array = np.genfromtxt(fname=current_dir.joinpath("elec.csv"), delimiter=",")
-#  %%
 
+#Load new testdata with RGI:
+elec_RGI_test_array = np.genfromtxt(fname=current_dir.joinpath("elec_RGI.csv"), delimiter=",")
+
+#  %% Plotting 
 fig, axs = pyplot.subplots(nrows=1, ncols=1)
 
 
-# np.testing.assert_allclose(actual=regular_interpolator_output, desired=elec_test_array)
-
-
 axs.plot(
-    regular_interpolator_output,
+    RGI_output,
     "k--",
 )
-axs.plot(elec_test_array, "b")
+axs.plot(elec_test_array, "b", linewidth=0.5)
 
 
 fig.show()
 
-pass
+perc_deviation = (elec_test_array - RGI_output) / elec_test_array * 100
+perc_deviation.mean() #-0.015562724915809874%
+perc_deviation.max() #0.6990781200055962%
 
-# %%
+# %% Testing
+
+np.testing.assert_allclose(actual=RGI_output, desired=elec_RGI_test_array)
+
